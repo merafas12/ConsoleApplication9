fbb0ccec4205e6da3ae6feb756a4fd7b6f49541f
merafas12 <merafastwitch1880@gmail.com>
2025-01-24 19:12:39 +0200

    update

--- ConsoleApplication9/Computer.h
+++ ConsoleApplication9/Computer.h
@@ -2,21 +2,21 @@
 #include <iostream>
 #include <cstring>
 #include <iomanip>
-
 using namespace std;
+
 struct Computer {
-	char name[50] = "undefind";
+	char name[50] = "undefined";
 	float frequency = 1.2;
 	int RAM = 4;
 	bool DVD_ROM = false;
 	float price = 5000;
 
 	void showInfo() {
-		cout << setw(20) < "Name" << name << endl;
-		cout << setw(20) < "frequency" << frequency << "GHz" << endl;
-		cout << setw(20) < "RAM" << RAM << "GB" <<  endl;
-		cout << setw(20) < "DVD_ROM" << boolalpha << DVD_ROM << endl;
-		cout << setw(20) < "Price" << price << "UAH" << endl;
-		cout << "__________________________________________________________________________\n"
+		cout << setw(20) << "Name: " << name << endl;
+		cout << setw(20) << "Frequency: " << frequency << "GHz" << endl;
+		cout << setw(20) << "RAM: " << RAM << "Gb" << endl;
+		cout << setw(20) << "DVD_ROM: " << boolalpha << DVD_ROM << endl;
+		cout << setw(20) << "Price: " << price << "UAH" << endl;
+		cout << "-----------------------------------------------\n";
 	}
 };
--- ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9.cpp
@@ -1,8 +1,17 @@
-﻿
-#include <iostream>
-#include <Computer.h>
+﻿#include "Computer.h"
+
 int main()
 {
-    std::cout << "Hello World!\n";
-}
+    Computer* arr = new Computer[5]{
+        {"HP", 2.4, 8, false, 20'000},
+        {"Acer", 3.2, 16, true, 60'000},
+        {"HP 2.0", 1.4, 8, false, 15'000},
+        {"Dell", 3.4, 32, true, 75'000},
+        {"Asus", 2.6, 16, true, 35'000}
+    };
 
+    for (int i = 0; i < 5; i++)
+    {
+        arr[i].showInfo();
+    }
+}
